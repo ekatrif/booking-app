e2f1cd383e7dcecedcb44c75d8b60c0c7d459089
Ekaterina Trifonova <ekaterinatrifonova@Ekaterinas-MacBook-Pro.local>
2024-01-08 12:49:59 +0300

    feat: add different metadata to pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from './components/NavBar'
 import './globals.css'
 
 export default function RootLayout({
@@ -7,12 +8,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Default description" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
+        <main className="bg-gray-100 min-h-screen w-screen">
+          <main className="max-w-screen-2xl m-auto bg-white">
+            <NavBar />
+            {children}
+          </main>
+        </main>
+        </body>
     </html>
   )
 }
+
+export const metadata = {
+  title: 'Open Table',
+  description: 'Main page description',
+};
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
-import NavBar from "../../components/NavBar";
-import Header from "./components/Header";
+
+
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -11,11 +10,7 @@ import ReservationCard from "./components/ReservationCard";
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-  <main className="max-w-screen-2xl m-auto bg-white">
-    <NavBar />
-    <Header />
-    <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+    <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavBar />
           <Title />
@@ -27,8 +22,7 @@ export default function RestaurantDetails() {
       <div className="w-[27%] relative text-reg">
         <ReservationCard />
       </div>
-    </div>
-  </main>
-</main>
+    </>
+
   )
 }
