adcef53c69e8088c013564929a7735285ac7eb44
Ekaterina Trifonova <ekaterinatrifonova@Ekaterinas-MacBook-Pro.local>
2024-01-09 09:45:21 +0300

    feat: add fetching restaurants data

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,11 +1,16 @@
-export default function Header() {
+export default function Header({ title }: { title: string}) {
+  const renderTitle = (slug: string) => {
+    const nameArray = slug.split('-');
+    nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`;
+    return nameArray.join(' ');
+  }
   return (
     <div className="h-96 overflow-hidden">
       <div
         className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center"
       >
-      <h1 className="text-7xl text-white captitalize text-shadow text-center">
-        Milestones Grill (Toronto)
+      <h1 className="text-7xl text-white capitalize text-shadow text-center">
+        {renderTitle(title)}
       </h1>
     </div>
   </div>
--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header";
 
-export default function  ({
+export default function RestaurantLayout ({
   children,
+  params,
 }: {
   children: React.ReactNode
+  params: {
+    slug: string
+  }
 }) {
   return (
     <>
-      <Header />
+      <Header title={params.slug} />
       <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
         {children}
       </div>
